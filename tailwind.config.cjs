2c4b7a2ef46fe6f27ed34dd4e257731c17127165
GustBitencourt <gubiten@gmail.com>
2022-10-13 20:37:43 -0300

    Componente e documentação do TextInput

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,16 +6,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        transparent: 'transparent',
         black: '#000',
         white: '#FFF',
-
-        'gray-900': '#121214',
-        'gray-800': '#202024',
-        'gray-400': '#7C7C8A',
-        'gray-200': '#C4C4CC',
-        'gray-100': '#E1E1E6',
-        'cyan-500': '#81D8F7',
-        'cyan-300': '#9CE5FF',
+        gray: {
+          900: '#121214',
+          800: '#202024',
+          400: '#7C7C8A',
+          200: '#C4C4CC',
+          100: '#E1E1E6',
+        },
+        cyan: {
+          500: '#81D8F7',
+          300: '#9CE5FF',
+        },
       },
       fontSize: {
         xs: 14,
