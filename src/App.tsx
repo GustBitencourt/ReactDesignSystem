496ee79f0bb71195a494fab2984ee0861328be83
GustBitencourt <gubiten@gmail.com>
2022-10-14 14:58:14 -0300

    Ajuste para adicionar className nos componentes de Heading and Text, header da pagina finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Heading } from './components/Heading'
+import { Logo } from './components/Logo'
+import { Text } from './components/Text'
+
 import './styles/global.css'
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Design System</h1>
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+      <header className="flex flex-col items-center">
+        <Logo />
+
+        <Heading size='lg' className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text size='lg' className="text-gray-400 mt-1">
+          Logue e comece a usar
+        </Text>
+      </header>
     </div>
   )
 }
